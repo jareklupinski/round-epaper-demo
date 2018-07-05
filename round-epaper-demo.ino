a120edaeb692f8fba43f2e37013dbcbf2b46046f
Jarek Lupinski <jarek319@gmail.com>
2018-07-05 19:40:42 -0400

    work in progress

---
+++ round-epaper-demo.ino
@@ -0,0 +1,33 @@
+#define SDA 13
+#define SCL 14
+#define CS 15
+#define DC 4
+#define RST 2
+#define BSY 5
+
+void sendData() {
+  digitalWrite(CS, LOW);
+  digitalWrite(DC, HIGH);
+  digitalWrite(SCL, LOW);
+}
+
+void sendCommand() {
+  digitalWrite(CS, LOW);
+  digitalWrite(DC, LOW);
+  digitalWrite(SCL, LOW);
+}
+
+void setup() {
+  pinMode(SDA, OUTPUT);
+  pinMode(SCL, OUTPUT);
+  pinMode(CS, OUTPUT);
+  pinMode(DC, OUTPUT);
+  pinMode(RST, OUTPUT);
+  pinMode(BSY, INPUT_PULLUP);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
